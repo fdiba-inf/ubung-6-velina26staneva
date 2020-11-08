954472c94f06df5c450a151179e47ec72ecc4202
velina26staneva <velin4eto01@gmail.com>
2020-11-08 19:35:23 +0000

    1

--- src/exercise6/Palindrome.java
+++ src/exercise6/Palindrome.java
@@ -5,5 +5,30 @@ import java.util.Scanner;
 public class Palindrome {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
+
+        System.out.print("Enter a number: ");
+        int number = input.nextInt(); 
+        int realInteger = number; 
+        int reversedInteger = 0;
+        boolean Palindrome;
+      
+        assert number > 0;
+        
+        int count = 0;
+
+        while (number > 0) {
+          count = number%10;
+          reversedInteger = reversedInteger*10 + count;
+          number = number / 10;
+        }
+        if (realInteger == reversedInteger ) {
+          Palindrome = true;
+          System.out.println("Palindrome: " + Palindrome);
+        }else {
+          Palindrome = false;
+          System.out.println("Palindrome: " + Palindrome );
+        }
+
+
     }
 }
